bc7cefa7e33042ed56d01ed00e841ba80cfd7f25
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2025-08-02 02:08:02 +0000

    calendariosemana

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Horarios from '@/components/Horarios/Horarios'
+import CalendarioSemanal from '@/components/CalendarioSemanal/CalendarioSemanal'
 
 export default function page() {
   return <>
     <Horarios></Horarios>
+    <CalendarioSemanal></CalendarioSemanal>
   </>
 }
