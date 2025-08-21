69434f8911edd9086abd11dbbcd6aaa73bac2b7f
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2025-08-21 17:30:31 +0000

    fonte do select mais pequena

--- components/SelectHorario/SelectHorario.tsx
+++ components/SelectHorario/SelectHorario.tsx
@@ -41,7 +41,7 @@ export default function SelectHorario({ onSelect }: SelectHorarioProps) {
     <select
       value={selectedHorarioId}
       onChange={handleHorarioSelection}
-      className="border rounded p-2 font-bold text-2xl cursor-pointer mb-2 flex row gap-3 items-center"
+      className="border rounded p-2 font-bold text-xl cursor-pointer mb-2 flex row gap-3 items-center"
       style={{ border: '1px solid lightgray' }}
     >
       <option value="">Selecione um Curso & Ano...</option>
